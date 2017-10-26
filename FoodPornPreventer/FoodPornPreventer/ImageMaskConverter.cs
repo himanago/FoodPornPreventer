7ee02c5ec471bb8d86449e09a360dfd1380a1892
T.Hirabayashi <baa_ram_ewe_shipping@live.jp>
2017-10-26 22:06:21 +0900

    画像差し替えを実装

---
+++ FoodPornPreventer/FoodPornPreventer/ImageMaskConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FoodPornPreventer
+{
+    public class ImageMaskConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // 飯テロ画像なら画像を差し替え
+            var imageInfo = value as TweetImageInfo;
+
+            if (imageInfo == null) return null;
+
+            return imageInfo.IsFoodPorn
+                ? ImageSource.FromResource("FoodPornPreventer.Images.mark_chuui.png")
+                : ImageSource.FromUri(new Uri(imageInfo.Url));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- FoodPornPreventer/FoodPornPreventer/MainPage.xaml.cs
+++ FoodPornPreventer/FoodPornPreventer/MainPage.xaml.cs
@@ -31,78 +31,73 @@ namespace FoodPornPreventer
 
         private async void TweetList_ItemAppearing(object sender, ItemVisibilityEventArgs e)
         {
-            try
+            if (TimelineEntities.Last() == e.Item as SimpleTimelineEntity)
             {
-                if (TimelineEntities.Last() == e.Item as SimpleTimelineEntity)
-                {
-                    await AddTweets(TimelineEntities.Last().Id);
-                }
-            }
-            catch (Exception ex)
-            {
+                await AddTweets(TimelineEntities.Last().Id);
             }
         }
 
         protected override async void OnAppearing()
         {
-            try
-            {
-                // タイムラインの取得とリストビューへの表示
-                await AddTweets(null);
-            }
-            catch (Exception ex)
-            {
-            }
+            // タイムラインの取得とリストビューへの表示
+            await AddTweets(null);
         }
 
         private async Task AddTweets(long? maxId = null)
         {
-            var timeline = await Tokens.Statuses.HomeTimelineAsync(count: TweetCount, max_id: maxId);
-
-            // リストビューへの表示
-            foreach (var item in timeline)
+            try
             {
-                var timelineEntity = new SimpleTimelineEntity
-                {
-                    Id = item.Id,
-                    IsRetweet = item.RetweetedStatus != null
-                };
+                var timeline = await Tokens.Statuses.HomeTimelineAsync(count: TweetCount, max_id: maxId);
 
-                // リツイートかどうかで情報をわける
-                if (timelineEntity.IsRetweet)
+                // リストビューへの表示
+                foreach (var item in timeline)
                 {
-                    // リツイートの場合は元ツイートの内容を格納
-                    timelineEntity.TweetText = item.RetweetedStatus.Text;
-                    timelineEntity.UserName = item.RetweetedStatus.User.Name;
-                    timelineEntity.UserProfileImageUrl = item.RetweetedStatus.User.ProfileImageUrl;
-                    timelineEntity.CreatedAt = item.RetweetedStatus.CreatedAt.LocalDateTime.ToString("F");
-                    timelineEntity.RetweetMessage = $"{item.User.Name}さんがリツイート";
-                }
-                else
-                {
-                    // それ以外はそのまま格納
-                    timelineEntity.TweetText = item.Text;
-                    timelineEntity.UserName = item.User.Name;
-                    timelineEntity.UserProfileImageUrl = item.User.ProfileImageUrl;
-                    timelineEntity.CreatedAt = item.CreatedAt.LocalDateTime.ToString("F");
-                };
-
-                if (item.ExtendedEntities != null)
-                {
-                    var tweetImages = new List<TweetImageInfo>();
-                    foreach (var media in item.ExtendedEntities.Media)
+                    var timelineEntity = new SimpleTimelineEntity
                     {
-                        tweetImages.Add(new TweetImageInfo
-                        {
-                            Url = media.MediaUrl,
-                            IsFoodPorn = await IsFoodPornAsync(media.MediaUrl)
-                        });
+                        Id = item.Id,
+                        IsRetweet = item.RetweetedStatus != null
+                    };
+
+                    // リツイートかどうかで情報をわける
+                    if (timelineEntity.IsRetweet)
+                    {
+                        // リツイートの場合は元ツイートの内容を格納
+                        timelineEntity.TweetText = item.RetweetedStatus.Text;
+                        timelineEntity.UserName = item.RetweetedStatus.User.Name;
+                        timelineEntity.UserProfileImageUrl = item.RetweetedStatus.User.ProfileImageUrl;
+                        timelineEntity.CreatedAt = item.RetweetedStatus.CreatedAt.LocalDateTime.ToString("F");
+                        timelineEntity.RetweetMessage = $"{item.User.Name}さんがリツイート";
                     }
-                    // Twitterの画像は最大4枚なので要素数は4固定
-                    timelineEntity.TweetImages = new TweetImageInfo[4];
-                    Array.Copy(tweetImages.ToArray(), timelineEntity.TweetImages, tweetImages.Count());                  
+                    else
+                    {
+                        // それ以外はそのまま格納
+                        timelineEntity.TweetText = item.Text;
+                        timelineEntity.UserName = item.User.Name;
+                        timelineEntity.UserProfileImageUrl = item.User.ProfileImageUrl;
+                        timelineEntity.CreatedAt = item.CreatedAt.LocalDateTime.ToString("F");
+                    };
+
+                    if (item.ExtendedEntities != null)
+                    {
+                        var tweetImages = new List<TweetImageInfo>();
+                        foreach (var media in item.ExtendedEntities.Media)
+                        {
+                            tweetImages.Add(new TweetImageInfo
+                            {
+                                Url = media.MediaUrl,
+                                IsFoodPorn = await IsFoodPornAsync(media.MediaUrl)
+                            });
+                        }
+                        // Twitterの画像は最大4枚なので要素数は4固定
+                        timelineEntity.TweetImages = new TweetImageInfo[4];
+                        Array.Copy(tweetImages.ToArray(), timelineEntity.TweetImages, tweetImages.Count());
+                    }
+                    TimelineEntities.Add(timelineEntity);
                 }
-                TimelineEntities.Add(timelineEntity);
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("警告", "ツイートの読み込みに失敗しました。", "閉じる");
             }
         }
 
